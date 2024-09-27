2d8b1f2889d0ab2ed19811938cc7d5ce6064400b
Krystian Kozak <whisky2332@gmail.com>
2024-09-27 18:11:32 +0200

    Landing page finished

--- frontend/src/pages/landingPage/components/JoinCard.tsx
+++ frontend/src/pages/landingPage/components/JoinCard.tsx
@@ -1,25 +1,24 @@
 import {Text, Flex, Box, Image} from "@chakra-ui/react"
 
+import signup from "../../../assets/signup.png"
 
 export const JoinCard = () => {
     return(
         <Box p={"4rem"} width={"100%"} mt={"8rem"}>
-            <Flex flexDirection={"row"} width={"100%"} alignItems={"center"}>
-            <Text color="#ECF0F1" fontSize="1.8rem" fontWeight="400" mt="1rem">
+            <Flex flexDirection={"column"} width={"100%"} alignItems={"center"}>
+            <Text color="#ECF0F1" fontSize="5rem" fontWeight="400" mb="2rem">
                 Join{" "}
                 <Text as="span" color="#1ABC9C" fontWeight="700">
-                  Cash Track 
+                  Cash Track {" "}
                 </Text>
                  Today!
               </Text>
+              <Flex flexDirection={"row"} width={"100%"} alignItems={"center"}>
             </Flex>
-            <Flex flexDirection={"row"} width={"100%"} alignItems={"center"}>
-            
               <Text color="#ECF0F1"
               fontSize="1.8em"
               fontWeight="400"
-              textAlign="center"
-              mb={"2rem"}>
+              textAlign="center">
                 Start your journey towards financial freedom with a tool that simplifies money management.
               </Text>
               <Text color="#ECF0F1"
@@ -28,6 +27,18 @@ export const JoinCard = () => {
               textAlign="center"
               mb={"2rem"}> Sign up now and see how easy it is to take control of your finances!</Text>
             </Flex>
+            <Flex flexDirection={"column"} width={"100%"} alignItems={"center"}>
+              <Image src={signup} alt="Signup button"/>
+              </Flex>
+              <Text color="#ECF0F1"
+              fontSize="1.8em"
+              fontWeight="400"
+              textAlign="center"
+              mb={"2rem"}>Or {" "}<Text as="span" color="#1ABC9C" fontWeight="700">
+              Login {" "}
+            </Text>
+              if you already have an account.
+             </Text>
         </Box>
     )
 }
