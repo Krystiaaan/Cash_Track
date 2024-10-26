d5418002bf4db77dc0caaae37b4ef7c2901cf340
Krystian Kozak <whisky2332@gmail.com>
2024-10-26 17:23:54 +0200

    RegisterCard with floatingLabel. TODO: Fix how errors are shown. spacing

--- frontend/src/pages/registerPage/components/RegisterCard.tsx
+++ frontend/src/pages/registerPage/components/RegisterCard.tsx
@@ -14,7 +14,6 @@ import { Formik, Form, Field } from "formik";
 import * as Yup from "yup";
 
 export const RegisterCard = () => {
-  
   return (
     <Formik
       initialValues={{
@@ -42,7 +41,7 @@ export const RegisterCard = () => {
           .oneOf(["Eur", "Usd"], "Currency must be either EUR or USD")
           .required("Currency is required"),
       })}
-      onSubmit={async (values, {setSubmitting}) => {
+      onSubmit={async (values, { setSubmitting, setErrors }) => {
         try {
           const response = await fetch("http://localhost:3000/auth/register", {
             method: "POST",
@@ -51,17 +50,27 @@ export const RegisterCard = () => {
             },
             body: JSON.stringify({
               username: values.username,
-              email:values.email,
+              email: values.email,
               passwordHash: values.password,
               currency: values.currency,
             }),
           });
-          if(response.ok){
+          if (response.ok) {
             alert("Registration successful!");
+          }else {
+            const errorData : {errors?: string[]} = await response.json();
+            if(errorData.errors){
+              const formErrors: { email?: string; username?: string } = {};
+              errorData.errors.forEach((error: string) => {
+                if (error.includes("Email")) formErrors.email = "This email is already in use.";
+                if (error.includes("Username")) formErrors.username = "This username is already taken.";
+              });
+              setErrors(formErrors);
+            }
           }
-        } catch (err){
+        } catch (err) {
           console.error("Registration failed:", err);
-        }finally{
+        } finally {
           setSubmitting(false);
         }
       }}
@@ -81,122 +90,175 @@ export const RegisterCard = () => {
               textAlign={"center"}
               borderRadius={"2rem"}
             >
-              <Heading mb={2} mt={"2rem"} color={"white"}>
+              <Heading mb={"3rem"} mt={"2rem"} color={"white"}>
                 Register
               </Heading>
 
               {/* Username Field */}
-              <FormControl
-                isInvalid={formik.touched.username && !!formik.errors.username}
-              >
-                <FormLabel></FormLabel>
-                <Field
-                  name="username"
-                  as={Input}
-                  placeholder="Your Username"
-                  bg={"#D9D9D9"}
-                  width={"25rem"}
-                  borderRadius={"1.5rem"}
-                  textAlign="center"
-                  mb="1.5rem"
-                  mt="2rem"
-                />
-                <FormErrorMessage>{formik.errors.username}</FormErrorMessage>
+              <FormControl isInvalid={formik.touched.username && !!formik.errors.username} mb="2rem" position="relative">
+                <Box pos="relative">
+                  <Field as={Input}  width={"25rem"} borderRadius={"1.5rem"} name="username" className="peer" placeholder="" bg={"#D9D9D9"} />
+                  <FormLabel
+                    sx={{
+                      pos: "absolute",
+                      px: "4rem",
+                      top: "-7",
+                      fontWeight: "normal",
+                      transition: "0.2s",
+                      insetStart: "2",
+                      _peerPlaceholderShown: {
+                        color: "black",
+                        top: "2.5",
+                        insetStart: "2",
+                      },
+                      _peerFocusVisible: {
+                        color: "black",
+                        top: "-7",
+                        insetStart: "2",
+                      },
+                    }}
+                  >
+                    Enter your Username:
+                  </FormLabel>
+                </Box>
+                <Box position="absolute" top="100%" left="0" width="100%" mb="1.5rem">
+                <FormErrorMessage ml="4.5rem" color="red.500" fontSize="sm">{formik.errors.username}</FormErrorMessage>
+                </Box>
               </FormControl>
 
               {/* Email Field */}
-              <FormControl
-                isInvalid={formik.touched.email && !!formik.errors.email}
-              >
-                <FormLabel></FormLabel>
-                <Field
-                  name="email"
-                  as={Input}
-                  placeholder="Your Email"
-                  bg={"#D9D9D9"}
-                  width={"25rem"}
-                  borderRadius={"1.5rem"}
-                  textAlign={"center"}
-                  mb="1.5rem"
-                />
-                <FormErrorMessage>{formik.errors.email}</FormErrorMessage>
+              <FormControl isInvalid={formik.touched.email && !!formik.errors.email} mb="2rem" position="relative">
+                <Box pos="relative">
+                  <Field as={Input} width={"25rem"} borderRadius={"1.5rem"} name="email" className="peer" placeholder="" bg={"#D9D9D9"} />
+                  <FormLabel
+                    sx={{
+                      pos: "absolute",
+                      px: "4rem",
+                      top: "-7",
+                      fontWeight: "normal",
+                      transition: "0.2s",
+                      insetStart: "2",
+                      _peerPlaceholderShown: {
+                        color: "black",
+                        top: "2.5",
+                        insetStart: "2",
+                      },
+                      _peerFocusVisible: {
+                        color: "black",
+                        top: "-7",
+                        insetStart: "2",
+                      },
+                    }}
+                  >
+                    Enter your Email:
+                  </FormLabel>
+                </Box>
+                <Box position="absolute" top="100%" left="0" width="100%" mt="0.5rem">
+                <FormErrorMessage ml="4.5rem" color="red.500" fontSize="sm">{formik.errors.email}</FormErrorMessage>
+                </Box>
               </FormControl>
 
               {/* Password Field */}
-              <FormControl
-                isInvalid={formik.touched.password && !!formik.errors.password}
-              >
-                <FormLabel></FormLabel>
-                <Field
-                  name="password"
-                  as={Input}
-                  placeholder="Your Password"
-                  type="password"
-                  bg={"#D9D9D9"}
-                  width={"25rem"}
-                  borderRadius={"1.5rem"}
-                  textAlign={"center"}
-                  mb="1.5rem"
-                />
-                <FormErrorMessage>{formik.errors.password}</FormErrorMessage>
+              <FormControl isInvalid={formik.touched.password && !!formik.errors.password} mb="2rem" position="relative">
+                <Box pos="relative">
+                  <Field as={Input} width={"25rem"} borderRadius={"1.5rem"} name="password" type="password" className="peer" placeholder="" bg={"#D9D9D9"} />
+                  <FormLabel
+                    sx={{
+                      pos: "absolute",
+                      px: "4rem",
+                      top: "-7",
+                      fontWeight: "normal",
+                      transition: "0.2s",
+                      insetStart: "2",
+                      _peerPlaceholderShown: {
+                        color: "black",
+                        top: "2.5",
+                        insetStart: "2",
+                      },
+                      _peerFocusVisible: {
+                        color: "black",
+                        top: "-7",
+                        insetStart: "2",
+                      },
+                    }}
+                  >
+                    Enter your Password:
+                  </FormLabel>
+                </Box>
+                <Box position="absolute" top="100%" left="0" width="100%" mt="0.5rem">
+                <FormErrorMessage ml="4.5rem" color="red.500" fontSize="sm">{formik.errors.password}</FormErrorMessage>
+                </Box>
               </FormControl>
 
               {/* Confirm Password Field */}
-              <FormControl
-                isInvalid={
-                  formik.touched.conf_pass && !!formik.errors.conf_pass
-                }
-              >
-                <FormLabel></FormLabel>
-                <Field
-                  name="conf_pass"
-                  as={Input}
-                  placeholder="Confirm your Password"
-                  type="password"
-                  bg={"#D9D9D9"}
-                  width={"25rem"}
-                  borderRadius={"1.5rem"}
-                  textAlign={"center"}
-                  mb="1.5rem"
-                />
-                <FormErrorMessage>{formik.errors.conf_pass}</FormErrorMessage>
+              <FormControl isInvalid={formik.touched.conf_pass && !!formik.errors.conf_pass} mb="2rem" position="relative">
+                <Box pos="relative">
+                  <Field as={Input} width={"25rem"} borderRadius={"1.5rem"} name="conf_pass" type="password" className="peer" placeholder="" bg={"#D9D9D9"} />
+                  <FormLabel
+                    sx={{
+                      pos: "absolute",
+                      px: "4rem",
+                      top: "-7",
+                      fontWeight: "normal",
+                      transition: "0.2s",
+                      insetStart: "2",
+                      _peerPlaceholderShown: {
+                        color: "black",
+                        top: "2.5",
+                        insetStart: "2",
+                      },
+                      _peerFocusVisible: {
+                        color: "black",
+                        top: "-7",
+                        insetStart: "2",
+                      },
+                    }}
+                  >
+                    Confirm your Password:
+                  </FormLabel>
+                </Box>
+                <Box position="absolute" top="100%" left="0" width="100%" mt="0.5rem">
+                <FormErrorMessage ml="4.5rem" color="red.500" fontSize="sm">{formik.errors.conf_pass}</FormErrorMessage>
+                </Box>
               </FormControl>
 
               {/* Currency Field */}
-              <FormControl
-                isInvalid={formik.touched.currency && !!formik.errors.currency}
-              >
-                <FormLabel></FormLabel>
+              <FormControl isInvalid={formik.touched.currency && !!formik.errors.currency} mb="2rem" position="relative">
                 <Field
                   name="currency"
                   as={Select}
                   placeholder="Select your Currency"
                   bg={"#D9D9D9"}
-                  width={"25rem"}
                   borderRadius={"1.5rem"}
-                  mx="auto"
-                  mb="1.5rem"
+                  width={"25rem"}
+                  textAlign={"center"}
+                  ml={"4rem"}
                 >
                   <option value="Eur">EUR</option>
                   <option value="Usd">USD</option>
                 </Field>
-                <FormErrorMessage>{formik.errors.currency}</FormErrorMessage>
+                <Box position="absolute" top="100%" left="0" width="100%" mt="0.5rem">
+                <FormErrorMessage ml="4.5rem" color="red.500" fontSize="sm">{formik.errors.currency}</FormErrorMessage>
+                </Box>
               </FormControl>
 
               {/* Submit Button */}
-              <Button
-                value="Register"
-                type="submit"
-                width={"25rem"}
-                borderRadius={"1.5rem"}
-                bg={"#2C3E50"}
-                color={"white"}
-                mb="1rem"
-                isLoading={formik.isSubmitting}
-              >
-                Register
-              </Button>
-                
+              <Box width="100%" display="flex" justifyContent="center">
+                <Button
+                  type="submit"
+                  width={"25rem"}
+                  minWidth={"25rem"}
+                  borderRadius={"1.5rem"}
+                  bg={"#2C3E50"}
+                  color={"white"}
+                  mb="1rem"
+                  mt={"1rem"}
+                  isLoading={formik.isSubmitting}
+                >
+                  Register
+                </Button>
+              </Box>
+
               <Text color={"white"}>
                 Already have an Account?{" "}
                 <Text as={"span"} color={"#2C3E50"} style={{ cursor: "pointer" }}>
