6805a0baf8bba251347369ecfadf09af9c75b7bf
Krystian Kozak <whisky2332@gmail.com>
2024-10-27 18:37:30 +0100

    Initial MainPage with import of MainCard

---
+++ frontend/src/pages/mainPage/components/MainCard.tsx
@@ -0,0 +1,8 @@
+
+
+
+export const MainCard = () => {
+    return (
+        <></>
+    )
+}
---
+++ frontend/src/pages/mainPage/mainPage.tsx
@@ -0,0 +1,12 @@
+import { BaseLayout } from "../../layout/baseLayout"
+import { MainCard } from "./components/MainCard"
+
+export const MainPage = () => {
+
+    return (
+        <BaseLayout>
+        <MainCard/>
+        </BaseLayout>
+    )
+
+}
